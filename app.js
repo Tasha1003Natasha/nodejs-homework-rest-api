74a6b7c1a26e96bebbb7afda32f04c54266a521d
Tasha1003Natasha <0955712824n@gmail.com>
2022-09-24 21:12:11 +0300

    process.env

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const mongoose = require("mongoose");
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
