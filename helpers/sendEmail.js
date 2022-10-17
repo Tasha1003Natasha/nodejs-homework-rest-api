f277b86382899dc622fe0f5e314ca4c299a7413c
Tasha1003Natasha <0955712824n@gmail.com>
2022-10-17 21:39:09 +0300

    Правка email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,11 +1,11 @@
 const sgMail = require("@sendgrid/mail");
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const mail = { ...data, from: "0955712824n@gmail.com" };
+  const mail = { ...data, from: EMAIL };
   await sgMail.send(mail);
   return true;
 };
